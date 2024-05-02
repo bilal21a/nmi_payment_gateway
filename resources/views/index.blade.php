e7c06960ec0e668f15ee92297c59432eddab7ada
Bilal Mac <b.anwar21.a@gmail.com>
2024-05-02 23:33:04 +0500

    fix cvv issue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -178,25 +178,25 @@
                 <label for="expiry_year">Expiry Year*</label>
                 <select id="expiry_year" name="expiry_year" required>
                     <option value="2023">2023</option>
-                    <option value="2023">2024</option>
-                    <option value="2023">2025</option>
-                    <option value="2023">2026</option>
-                    <option value="2023">2027</option>
-                    <option value="2023">2028</option>
-                    <option value="2023">2029</option>
-                    <option value="2023">2030</option>
-                    <option value="2023">2031</option>
-                    <option value="2023">2032</option>
-                    <option value="2023">2033</option>
-                    <option value="2023">2034</option>
-                    <option value="2023">2035</option>
-                    <option value="2023">2036</option>
-                    <option value="2023">2037</option>
-                    <option value="2023">2038</option>
-                    <option value="2023">2039</option>
-                    <option value="2023">2040</option>
-                    <option value="2023">2041</option>
-                    <option value="2023">2042</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                    <option value="2027">2027</option>
+                    <option value="2028">2028</option>
+                    <option value="2029">2029</option>
+                    <option value="2030">2030</option>
+                    <option value="2031">2031</option>
+                    <option value="2032">2032</option>
+                    <option value="2033">2033</option>
+                    <option value="2034">2034</option>
+                    <option value="2035">2035</option>
+                    <option value="2036">2036</option>
+                    <option value="2037">2037</option>
+                    <option value="2038">2038</option>
+                    <option value="2039">2039</option>
+                    <option value="2040">2040</option>
+                    <option value="2041">2041</option>
+                    <option value="2042">2042</option>
                     <!-- Add other years -->
                 </select>
             </div>
